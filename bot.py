6bb35aee7606bdd14d80dfced1f91e633942a759
abolfazl <gsm2230aga@gmail.com>
2026-08-18 18:53:23 +0330

    Refactor bot.py for HTTPXRequest and naming updates
    Refactor bot.py to use HTTPXRequest for Telegram bot requests and update variable naming conventions.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from telegram import Bot
+from telegram.request import HTTPXRequest
 
 load_dotenv()
 
@@ -12,31 +13,43 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 TOTAL_QUESTIONS = 155
 DELAY = 1
 
-option_emojis = ["🔴", "🔵", "🟢", "🟡"]
+OPTION_EMOJIS = ["🔴", "🔵", "🟢", "🟡"]
+
 
 async def main():
     if not TOKEN or not CHANNEL_ID:
         raise ValueError("BOT_TOKEN or CHANNEL_ID is not set in .env")
 
-    bot = Bot(token=TOKEN)
+    request = HTTPXRequest(
+        read_timeout=30,
+        write_timeout=30,
+        connect_timeout=30,
+        pool_timeout=10,
+    )
+
+    bot = Bot(
+        token=TOKEN,
+        request=request,
+    )
 
     for question in range(1, TOTAL_QUESTIONS + 1):
 
         await bot.send_message(
-        chat_id=CHANNEL_ID,
-        text=f"📝 سؤال {question}:"
-    )
-    
-    await asyncio.sleep(DELAY)
-
-
-    for option in range(1, 5):
-        await bot.send_message(
             chat_id=CHANNEL_ID,
-            text=f"{option_emojis[option - 1]} گزینه {option}"
+            text=f"📝 سؤال {question}:"
         )
 
         await asyncio.sleep(DELAY)
+
+        for option in range(1, 5):
+
+            await bot.send_message(
+                chat_id=CHANNEL_ID,
+                text=f"{OPTION_EMOJIS[option - 1]} گزینه {option}"
+            )
+
+            await asyncio.sleep(DELAY)
+
     print("ارسال تمام شد.")
 
 
